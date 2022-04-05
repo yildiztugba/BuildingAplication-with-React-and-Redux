3cea3a5d890414d03af859a8070f83fd961072b3
yildiztugba <okra.yildiz@gmail.com>
2022-04-05 13:53:45 +0200

    bug:typo mistake

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -25,7 +25,7 @@ module.exports ={
     plugins:[
         new HtmlWebpackPlugin({
             template:"src/index.html",
-            favicon: "src.favicon.icon"
+            favicon: "src/favicon.ico"
         })
     ],
     module:{
