689187ad2b9ab1cc57ab0e12a55b8ca32546a47a
yildiztugba <okra.yildiz@gmail.com>
2022-04-06 13:00:56 +0200

    redux config completed

--- src/index.js
+++ src/index.js
@@ -4,10 +4,16 @@ import {BrowserRouter as Router} from 'react-router-dom';
 import "bootstrap/dist/css/bootstrap.css";
 import App from './components/App';
 import "./index.css";
+import configureStore from './redux/configureStore';
+import {Provider as ReduxProvider} from 'react-redux';
+const store= configureStore();
+
 
 render (
+    <ReduxProvider store={store}>
     <Router>
         <App /> 
-    </Router>,
+    </Router>
+    </ReduxProvider>,
     document.getElementById("app")    
 );
