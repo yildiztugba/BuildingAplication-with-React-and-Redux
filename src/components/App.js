b03faa801af0bf2199cf5e7ff42d19106505bb12
yildiztugba <okra.yildiz@gmail.com>
2022-04-06 12:30:59 +0200

    binding

--- src/components/App.js
+++ src/components/App.js
@@ -13,7 +13,7 @@ function App() {
             <Switch>
                 <Route exact path="/" component={HomePage} />
                 <Route path="/about" component={AboutPage} />
-                <Route path="/courses" component={CoursesPage} />
+                 <Route path="/courses" component={CoursesPage} />
                 <Route component={PageNotFound} />
             </Switch>
         
